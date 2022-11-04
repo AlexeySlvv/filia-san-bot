192e80ef7d5dff2167d847107193fca53b4a7f53
AlexeySlvv <me@alexeyslvv.ru>
2022-11-04 14:00:25 +0300

    move to aiogram and remove wordnet

--- filiasan.py
+++ filiasan.py
@@ -1,85 +1,40 @@
-from collections import defaultdict
-from typing import OrderedDict
-
-import argparse
-
-import nest_asyncio
-
-from telegram import Update
-from telegram.ext import filters, ApplicationBuilder, MessageHandler
-
-import nltk
-from nltk.corpus import wordnet as wn
-
 from translatepy.translators.google import GoogleTranslate
 from libretran_help import lt_translate
 
-parser = argparse.ArgumentParser(description='Filia-san telegram bot')
-parser.add_argument('-f', '--token', help='bot token file')
+from aiogram import Bot, types
+from aiogram.dispatcher import Dispatcher
+from aiogram.utils import executor
 
+import argparse
+
+parser = argparse.ArgumentParser(description='Filisia telegram bot')
+parser.add_argument('-t', '--token', help='Filisia token file')
 
 # token
 try:
     args = parser.parse_args()
     with open(args.token, 'r') as t_f:
         TOKEN = t_f.read()
+        bot = Bot(token=TOKEN)
+        dp = Dispatcher(bot)
 except Exception as e:
     print('Token file error:', str(e))
     exit()
 
 
-nltk.download('wordnet')
-nltk.download('omw-1.4')
-
-
-POS_DICT = OrderedDict({
-    'n': 'Noun',
-    'v': 'Verb',
-    'a': 'Adjective',
-    's': 'Adjective sattelite',
-    'r': 'Adverb',
-})
-
-
-nest_asyncio.apply()
-
-
 gtranslate = GoogleTranslate()
 
 
-def bot_do(text: str) -> str:
-    bot_text = str()
-
+@dp.message_handler()
+async def do_reply(msg: types.Message):
     # TODO from-to languages
-    # bot_text += f"{gtranslate.translate(text, 'Russian')}"
-    bot_text += f"{lt_translate(text, lang_to='ru')}"
 
-    # WordNet
-    synsets = wn.synsets(text)
-    if synsets:
-        syn_dict = defaultdict(list)
-        for syn in synsets:
-            syn_dict[syn.pos()].append(syn.definition())
+    lt_text = f"Libre translate:\n{lt_translate(msg.text, lang_to='ru')}"
+    await msg.reply(lt_text)
 
-        for pos in POS_DICT:
-            definitions = syn_dict.get(pos)
-            if definitions:
-                bot_text += f"\n\n{POS_DICT[pos]}\n"
-                for definition in definitions:
-                    bot_text += f'\n * {definition}\n'
-                bot_text += '\n'
-
-    return bot_text if bot_text else 'Sorry, nothing was found'
-
-
-async def reply(update: Update, context) -> None:
-    user_text = update.message.text.lower()
-    bot_reply = bot_do(user_text)
-    await update.message.reply_text(bot_reply)
+    gt_text = f"Google translate:\n{gtranslate.translate(msg.text, 'Russian')}"
+    await msg.reply(gt_text)
 
 
 if __name__ == '__main__':
-    app = ApplicationBuilder().token(TOKEN).build()
-    handler = MessageHandler(filters.Text(), reply)
-    app.add_handler(handler)
-    app.run_polling()
+    executor.start_polling(dp, skip_updates=True)
